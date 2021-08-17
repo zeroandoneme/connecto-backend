bdc2784af6edf4b77254bb39c5869abffb118dc6
Ahmad Edris <a.edris@zeroandone.me>
2021-08-17 16:12:10 +0300

    change response

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const app = express();
 const port = 5000;
 
 app.get("/", (req, res) => {
-  res.send("Hello from connecto backend.");
+  res.send("Hello from connecto backend v2.");
 });
 
 app.listen(port, (err) => {
